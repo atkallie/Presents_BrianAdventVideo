c2be350057fc67e48d91a9b49a5be6d65aa3bc84
Ahmed T Khalil <ahmedtkhalil@Ahmeds-MacBook-Pro.local>
2017-01-28 17:52:55 -0500

    Initial Commit

---
+++ Presents_BrianAdventVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Presents_BrianAdventVideo
+//
+//  Created by Ahmed T Khalil on 1/28/17.
+//  Copyright © 2017 kalikans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
